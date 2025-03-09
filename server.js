f2a445d7b9476d1fb56de6961d3f057c3cbc8d36
PITTAWAT <dev@dosetech.co>
2025-03-10 00:24:42 +0700

    update

--- server.js
+++ server.js
@@ -1,217 +1,369 @@
-const express = require('express')
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
-const cors = require('cors')
+const cors = require("cors");
 
-const moment = require('moment');
+const moment = require("moment");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'node-project',
-    port     : '8889'
+  host: "localhost",
+  user: "root",
+  password: null,
+  database: "lucky_draw",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("Error connect db !!")
-        return
-    }
-    console.log("Connect db success !!")
-    console.log("==========================")
-})
+  if (err) {
+    console.log("Error connect db !!");
+    return;
+  }
+  console.log("Connect db success !!");
+  console.log("==========================");
+});
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+/// login ///
+app.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  let arr = [name, password];
+  if (arr.includes(undefined))
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
 
-app.get('/home', async (req, res) => {
-    const { take, skip } = req.query
+  try {
+    let query = `SELECT * FROM user WHERE name = '${name}'`;
+    connection.query(query, (err, response) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Get data fail !!", payload: [], result: 0 });
+      }
 
-    try {
-        let query = "SELECT id, name, description, price, post_code FROM house WHERE delete_flag = '0'"
-        if (take && skip) query = `${query} LIMIT ${take} OFFSET ${skip}`
+      let date = new Date();
+      let newDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
 
-        connection.query(query,
-        (err, result) => {
+      if (response.length) {
+        try {
+          let query = `UPDATE user SET 
+                            active = '1' ,
+                            update_dt = '${newDate}'
+                            WHERE id = ${response[0].id}`;
+
+          connection.query(query, (err, result) => {
             if (err) {
-                console.log(err)
-                res.status(400).send({ message:"Get data fail !!"})
+              console.log(err);
+              res.status(400).send({
+                message: "Update data fail !!",
+                payload: [],
+                result: 0,
+              });
             }
-            res.status(200).json({ message: "Get Success !!", payload: result, count: result.length})
-        })
+            res.status(200).json({
+              message: "Login Success !!",
+              payload: response[0],
+              result: 1,
+            });
+          });
+        } catch (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ message: "Something wrong !!", payload: [], result: 0 });
+        }
+      } else {
+        try {
+          connection.query(
+            "INSERT INTO user(name, password, active, role, update_dt) VALUES(?, ?, ?, ?, ?)",
+            [name, password, 1, "user", newDate],
+            (err, data) => {
+              if (err) {
+                console.log(err);
+                res
+                  .status(400)
+                  .send({ message: "Login Error !!", payload: [], result: 0 });
+              }
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message: "Something wrong !!", payload: []})
-    }
-})
-
-///////  CREATE DATA  ///////
-app.post('/home', async (req, res) => {
-    const { name, desc, price, post_code } = req.body
-    let arr = [name, desc, price, post_code]
-    if (arr.includes(undefined)) return res.status(400).send({ message: "Invalid Data !!", payload: []})
-
-    let checkData = await checkInt([price, post_code])
-    if (!checkData) return res.status(400).send({ message: "Data type Error !!", payload: []})
-
-    try {
-        connection.query("INSERT INTO house(name, description, price, post_code) VALUES(?, ?, ?, ?)",
-        [name, desc, price, post_code],
-        (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send({ message:"Insert Error !!", payload: []})
-            }
-            res.status(200).json({ message: "Insert Success !!", payload: []})
-        })
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message:"Something wrong !!"})
-    }
-})
-
-app.get('/postCode', async (req, res) => {
-    try {
-        connection.query("SELECT DISTINCT post_code FROM house WHERE delete_flag = '0'",
-        (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send({ message:"Get data fail !!", payload: []})
-            }
-            res.status(200).json({ message: "Get Success !!", payload: result, count: result.length})
-        })
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message:"Something wrong !!", payload: []})
-    }
-})
-
-app.get('/postCode/:id', async (req, res) => {
-    const id = req.query.id
-    if (!id) return res.status(400).send({ message: "Invalid Data !!", payload: []})
-
-    try {
-        connection.query(`SELECT price FROM house WHERE post_code = ${id}`,
-        (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send({ message:"Get data fail !!", payload: []})
-            }
-
-            let arr = []
-            let { average, median } = 0
-            if (result.length > 0) {
-                for (const r of result) {
-                    arr.push(r.price)
-                }
-                let sum = arr.reduce((a, b) => a + b);
-                average = sum / arr.length
-
-                arr.sort((a, b) => a - b);
-
-                let half = Math.floor(arr.length / 2);
-                if (arr.length % 2) median = arr[half];
-                else median = (arr[half - 1] + arr[half]) / 2;
-
-                res.status(200).json({ message: "Get Success !!", payload: { average: average, median: median }})
-            } else res.status(200).json({ message: "Get Success (data not found) !!", payload: []})
-        })
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message:"Something wrong !!", payload: []})
-    }
-})
-
-///////  UPDATE DATA  ///////
-app.patch('/home/:id', async (req, res) => {
-    const id = req.query.id
-    if (!id) return res.status(400).send({ message: "Invalid Data !!", payload: []})
-
-    if (Object.keys(req.body).length === 0) return res.status(400).send({ message: "Invalid Data !!"})
-    const { name, desc, price, post_code } = req.body
-
-    let checkData = await checkInt([price, post_code])
-    if (!checkData) return res.status(400).send({ message: "Data type Error !!", payload: []})
-
-    try {
-        let date = new Date()
-        let newDate = moment(date).format('YYYY-MM-DD HH:mm:ss')
-
-        let query = `UPDATE house SET 
-                    ${name ? `name = '${name}',` : "" } 
-                    ${desc ? `description = '${desc}',` : "" } 
-                    ${price ? `price = '${price}',` : "" } 
-                    ${post_code ? `post_code = '${post_code}',` : "" }
-                    update_dt = '${newDate}'
-                    WHERE id = ${id}`
-
-        connection.query(query, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send({ message: "Update data fail !!", payload: []})
-            }
-
-            connection.query(`SELECT name, description, price, post_code FROM house WHERE id = ${id}`,
-            (err, result) => {
+              let query = `SELECT * FROM user WHERE id = '${data.insertId}'`;
+              connection.query(query, (err, newData) => {
                 if (err) {
-                    console.log(err)
-                    res.status(400).send({ message: "Get new data fail !!", payload: []})
+                  console.log(err);
+                  res.status(400).send({
+                    message: "Get data fail !!",
+                    payload: [],
+                    result: 0,
+                  });
                 }
 
-               res.status(200).json({ message: "Update Success !!", payload: result})
-            })
-        })
+                res.status(200).json({
+                  message: "Login Success !!",
+                  payload: newData[0],
+                  result: 1,
+                });
+              });
+            }
+          );
+        } catch (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ message: "Something wrong !!", payload: [], result: 0 });
+        }
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message:"Something wrong !!", payload: []})
-    }
-})
+/// logout ///
+app.post("/logout/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id)
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
 
-///////  DELETE DATA  ///////
-app.delete('/home/:id', async (req, res) => {
-    const id = req.query.id
-    if (!id) return res.status(400).send({ message: "Invalid Data !!", payload: []})
+  try {
+    let date = new Date();
+    let newDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
 
-    try {
-        let date = new Date()
-        let newDate = moment(date).format('YYYY-MM-DD HH:mm:ss')
+    let query = `UPDATE user SET event_id = null, active = '0', update_dt = '${newDate}' WHERE id = '${id}'`;
 
-        let query = `UPDATE house SET update_dt = '${newDate}', delete_flag = '1' WHERE id = '${id}'`
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Logout fail !!", payload: [], result: 0 });
+      }
 
-        connection.query(query, (err, result) => {
+      res
+        .status(200)
+        .json({ message: "Logout Success !!", payload: [], result: 1 });
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
+
+/// list activity ///
+app.get("/activity", async (req, res) => {
+  const { name, take, skip } = req.query;
+
+  try {
+    let query = `SELECT * FROM activity ${
+      name ? `WHERE name = '${name}'` : ""
+    }`;
+    if (take && skip) query = `${query} LIMIT ${take} OFFSET ${skip}`;
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Get data fail !!", payload: [], result: 0 });
+      }
+      res.status(200).json({
+        message: "Get Success !!",
+        payload: result,
+        count: result.length,
+        result: 1,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
+
+/// get activity detail ///
+app.get("/activity/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id)
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
+
+  try {
+    let query = `SELECT * FROM activity WHERE id = ${id}`;
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Get Data fail !!", payload: [], result: 0 });
+      }
+
+      let userList = [];
+      if (result[0].user_id) {
+        let query = `SELECT * FROM user WHERE id IN (${result[0].user_id})`;
+
+        connection.query(query, (err, response) => {
+          if (err) {
+            console.log(err);
+            res
+              .status(400)
+              .send({ message: "Get Data fail !!", payload: [], result: 0 });
+          }
+
+          userList = response;
+          result[0].userList = userList;
+          delete result[0].user_id;
+
+          res.status(200).json({
+            message: "Get Data Success !!",
+            payload: result[0],
+            result: 1,
+          });
+        });
+      } else {
+        delete result[0].user_id;
+        res.status(200).json({
+          message: "Get Data Success !!",
+          payload: result[0],
+          result: 1,
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
+
+/// update user join event ///
+app.post("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id)
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
+
+  if (Object.keys(req.body).length === 0)
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
+  const { event_id } = req.body;
+
+  try {
+    let date = new Date();
+    let newDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
+
+    let query = `UPDATE user SET 
+                    ${event_id ? `event_id = '${event_id}',` : ""}
+                    update_dt = '${newDate}'
+                    WHERE id = ${id}`;
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Update data fail !!", payload: [], result: 0 });
+      }
+
+      res
+        .status(200)
+        .json({ message: "Update Success !!", payload: [], result: 1 });
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
+
+/// random lucky ///
+app.get("/random/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id)
+    return res
+      .status(400)
+      .send({ message: "Invalid Data !!", payload: [], result: 0 });
+
+  try {
+    let query = `SELECT * FROM user WHERE role != 'admin'`;
+
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(400)
+          .send({ message: "Get data fail !!", payload: [], result: 0 });
+      }
+
+      if (result.length) {
+        let arrayLucky = [];
+        let userId = [];
+        let array = [...result];
+        let totalLucky = 10;
+        for (let i = 0; i < totalLucky; i++) {
+          const index = Math.floor(Math.random() * array.length);
+          let data = array.splice(index, 1)[0];
+          userId.push(data.id);
+          arrayLucky.push(data);
+        }
+
+        try {
+          let date = new Date();
+          let newDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
+
+          let query = `UPDATE activity SET
+                            flag_random = '1',
+                            update_dt = '${newDate}',
+                            user_id = '${userId}'
+                            WHERE id = ${id}`;
+
+          connection.query(query, (err, result) => {
             if (err) {
-                console.log(err)
-                res.status(400).send({ message: "Delete data fail !!", payload: []})
+              console.log(err);
+              res.status(400).send({
+                message: "Update Data fail !!",
+                payload: [],
+                result: 0,
+              });
             }
 
-            res.status(200).json({ message: "Delete data Success !!", payload: []})
-        })
+            res.status(200).json({
+              message: "Update Data Success !!",
+              payload: arrayLucky,
+              result: 1,
+            });
+          });
+        } catch (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ message: "Something wrong !!", payload: [], result: 0 });
+        }
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Something wrong !!", payload: [], result: 0 });
+  }
+});
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ message:"Something wrong !!", payload: []})
-    }
-})
-
-async function checkInt(...data) {
-    for (const d of data) {
-        if (!(d !== null && d !== "" && parseInt(d) == Number(parseInt(d)))) return false 
-    }
-    return true
-}
-
-app.listen(3000, ()=> {
-    console.log("==========================")
-    console.log("Node API app is running !!")
-})
+app.listen(4000, () => {
+  console.log("==========================");
+  console.log("Node API app is running !!");
+});
